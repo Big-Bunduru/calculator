f68aedbf1e75970f042dd6ae29aa804287f4a419
ebdet2002 <adarsh.r.ramakrishnan@gmail.com>
2020-05-10 08:51:32 -0400

    division first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const division = require ('./division')
+
+divisionResult = division(8,4)
+
+console.log(divisionResult)
---
+++ division.js
@@ -0,0 +1,6 @@
+const division = function(x,y) {
+    return x / y
+}
+
+
+module.exports = 'division'
