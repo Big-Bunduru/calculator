3a8beee2fda292fcb91903929d2fcd32cac2bf25
ebdet2002 <adarsh.r.ramakrishnan@gmail.com>
2020-05-10 09:34:10 -0400

    added subtract to app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const division = require('./division')
 const add = require('./add')
+const subtract = require('./subtract')
 
 const added = add.add(8,4)
-let divisionResult = division.division(8,4)
+let divided = division.division(8,4)
+const subtracted = subtract(5,9)
 
 console.log(added)
-console.log(divisionResult)
+console.log(divided)
+console.log(subtracted)
